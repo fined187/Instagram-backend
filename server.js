c0c927d8519c7db8f6b3161a08238ea209e0b1c8
fined000@naver.com <fined000@naver.com>
2023-02-19 19:01:37 +0900

    day2 commit

--- server.js
+++ server.js
@@ -1,49 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+require("dotenv").config();
 import { ApolloServer, gql } from "apollo-server";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),        //  db에서 모든 영화들을 검색함.
-    movie: (_, { id }) => client.movie.findUnique({where:{ id } })
-  },
-  Mutation: {
-    createMovie: (_, {title, year, genre}) => client.movie.create({data: {
-      title,
-      year,
-      genre
-    }}),
-    deleteMovie: (_, {id}) => client.movie.delete({where: { id }}),
-    updateMovie: (_, {year}) => client.movie.update({where: {id}, data: {year}})
-  }
-};
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema
 });
 
-server.listen().then(() => console.log("Server is runnung on http://localhost:4000"));
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`🚀Server is runnung on http://localhost:${PORT}`));
