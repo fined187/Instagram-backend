4e846b501a044c239b9aa611c11d3b390f325092
fined000@naver.com <fined000@naver.com>
2023-02-21 22:34:55 +0900

    day3 commit

--- Users/users.mutations.js
+++ Users/users.mutations.js
@@ -1,5 +1,6 @@
 import client from "../client.js";
 import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -39,6 +40,30 @@ export default {
       } catch(e) {
         return e;
       }
-    }
+    },
+    login: async(_, {userName, password}) => {
+      //  find user with args.userName
+      const user = await client.user.findFirst({where: {userName}});
+      if(!user) {
+        return {
+          ok: false,
+          error: "User not found"
+        };
+      }
+      const passwordOk = await bycrypt.compare(password, user.password);
+      if(!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password."
+        }
+      }
+      //  check password with args.password
+      const token = await jwt.sign({id: user.id}, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
+      //  issue a token and send it to the user
+    },
   }
 }
